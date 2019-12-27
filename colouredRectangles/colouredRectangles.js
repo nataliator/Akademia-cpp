f9536da9a2eaf26bcf997ee0a25ae6742b82847a
nataliator <58991014+nataliator@users.noreply.github.com>
2019-12-27 19:28:32 +0100

    division of the repository

--- colouredRectangles/colouredRectangles.js
+++
@@ -1,9 +0,0 @@
-const divs = document.querySelectorAll('div');
-
-divs.forEach(function (div) {
-    div.addEventListener('click', changeColor)
-});
-
-function changeColor() {
-    document.body.className = this.className;
-}
