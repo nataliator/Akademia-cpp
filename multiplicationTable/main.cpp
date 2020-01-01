d45e27c566c320a0de56c3c00ea4d50cab5a1780
nataliator <58991014+nataliator@users.noreply.github.com>
2020-01-01 23:16:51 +0100

    The multiplication table simply version

---
+++ multiplicationTable/main.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+#include <iomanip>
+
+using namespace std;
+
+
+
+int main()
+{
+    int num1;
+    int num2;
+    int userAns;
+    srand( time( NULL ));
+    num1 = 2 + (rand() % 8);
+    num2 = 2 + (rand() % (num1-2));
+
+    cout << "First number is: "<< num1 << endl;
+    cout << "Second number is: "<< num2 << endl;
+    cout << "Please multiply these numbers and enter the answer: " << endl;
+    cin >> userAns;
+    int product = num1*num2;
+    if (userAns == product){
+        cout << "CORRECT" << endl;
+    }else {
+        cout << "INCORRECT, correct value is: " << product << endl;
+    }
+
+    int counter = 0;
+    for(int i = 0; i < num2; i++){
+        for(int j = 0; j < num1; j++){
+            cout << setfill(' ') << setw(2) << ++counter << " ";
+        }
+    cout << endl;
+    }
+
+return 0;
+}
