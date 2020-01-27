351140de61e5921bb3364e7405f0bb3649522342
nataliator <58991014+nataliator@users.noreply.github.com>
2020-01-27 22:49:56 +0100

    Codility Perm Check

---
+++ codilityCountingElPermCheck/main.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int solution(vector<int> &A){
+    int Asize = A.size();
+    int permutationCheck[Asize + 1];
+    int counter = 0;
+    int result = 0;
+
+    for(int i = 0; i < Asize + 1;i++){
+        permutationCheck[i] = 0;
+    }
+    for(int i = 0;i < Asize; i++){
+        if(A[i] > 100000 || A[i] > Asize){
+            return result;
+            cout << result <<endl;
+            break;
+        }
+        if(permutationCheck[A[i]] ==0 ){
+            counter++;
+            cout << counter << "counter" <<endl;
+
+        }
+        permutationCheck[A[i]] = 1;
+        if(counter == Asize){
+            result = 1;
+        }
+    }
+return result;
+}
+
+
+int main()
+{
+    vector<int>A ={2,3,4,5,6,7,8,9,10,11,12,13,14,15};
+    cout << solution(A) << endl;
+    return 0;
+}
